0a4128105bb3029a745d4c5c2c8ff5f0df252c2d
Juniper Mills <47700808+gblrfgr@users.noreply.github.com>
2024-09-03 19:32:30 -0500

    Laid basic groundwork

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
+import argparse
+import logging
+import nominatim
+import polars as pl
+import json
+
+
 def main():
-    pass
+    parser = argparse.ArgumentParser(
+        prog="reverse-geocode",
+        description="Converts GeoJSON data to a CSV table with street addresses",
+    )
+    parser.add_argument("filename")
+    parser.add_argument("-o", "--output")
+    parser.add_argument("-v", "--verbose", action="store_true")
+
+    args = parser.parse_args()
+    print(args.filename)
 
 
 if __name__ == "__main__":
